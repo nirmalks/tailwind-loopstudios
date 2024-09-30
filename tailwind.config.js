47a175394706e55d5363c3eba97501a905255e23
nirmalks <kagosekar@gmail.com>
2024-09-30 08:40:28 +0530

    Add basic hamburger

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero': "url('./images/image-hero.jpg')",
+        'mobile-hero': "url('./images/mobile-image-hero.jpg')"
+      },
+      transformOrigin: {
+        'top-left-0-0': '0% 0%',
+        'bottom-left-0-100': '0% 100%'
+      }
+    },
   },
   plugins: [],
 }
